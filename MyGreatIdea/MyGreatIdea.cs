4136b5146400440573ac353ac0fb0deded2bf18d
zwc1986@gmail.com <zwc1986@gmail.com@80838901-73b8-fa99-a6e8-e4e6b433e692>
2011-11-09 03:59:13 +0000

    创建项目

---
+++ MyGreatIdea/MyGreatIdea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace name.zwc.Caller.Handlers
+{
+    public class MyGreatIdea
+    {
+        public static String Test(String input, Dictionary<String, String> args)
+        {
+            return "Test";
+        }
+    }
+}
