f855c9c84b64eab019b5c4cee07336a408fdfa09
zwc1986@gmail.com <zwc1986@gmail.com@80838901-73b8-fa99-a6e8-e4e6b433e692>
2011-11-09 08:47:53 +0000

    Adding method declarations

--- trunk/MyGreatIdea/MyGreatIdea.cs
+++ trunk/MyGreatIdea/MyGreatIdea.cs
@@ -6,7 +6,27 @@ namespace name.zwc.Caller.Handlers
 {
     public class MyGreatIdea
     {
-        public static String Test(String input, Dictionary<String, String> args)
+        public static String AddIdea(String input, Dictionary<String, String> args)
+        {
+            return "Test";
+        }
+        public static String AddComment(String input, Dictionary<String, String> args)
+        {
+            return "Test";
+        }
+        public static String GetIdeas(String input, Dictionary<String, String> args)
+        {
+            return "Test";
+        }
+        public static String GetMyIdeas(String input, Dictionary<String, String> args)
+        {
+            return "Test";
+        }
+        public static String GetIdeaDetail(String input, Dictionary<String, String> args)
+        {
+            return "Test";
+        }
+        public static String FeedBack(String input, Dictionary<String, String> args)
         {
             return "Test";
         }
