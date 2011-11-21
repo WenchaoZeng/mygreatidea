a63895846eddc1171de9d6cce0181e3f8d751057
zwc1986@gmail.com <zwc1986@gmail.com@80838901-73b8-fa99-a6e8-e4e6b433e692>
2011-11-21 08:47:17 +0000

    Add entity classes

---
+++ MyGreatIdeaAndroid/src/name/zwc/mygreatidea/android/entities/Idea.java
@@ -0,0 +1,12 @@
+package name.zwc.mygreatidea.android.entities;
+
+public class Idea
+{
+    public int Id;
+    public String Title;
+    public String Content;
+    public int PostTime;
+    public String UserName;
+    public String UserContact;
+    public int CommentCount;
+}
